9694f3662ca41baa0179ee85a1befdb5ac750fcd
DevCodeKing <contato@narutibia.com.br>
2026-02-21 15:05:07 -0300

    Fix: Compilation errors and update gitignore for build artifacts

--- src/otpch.h
+++ src/otpch.h
@@ -1,6 +1,7 @@
 // Copyright 2022 The Forgotten Server Authors. All rights reserved.
 // Use of this source code is governed by the GPL-2.0 License that can be found in the LICENSE file.
 
+#ifndef FS_OTPCH_H_F00C737DA6CA4C8D90F57430C614367F
 #define FS_OTPCH_H_F00C737DA6CA4C8D90F57430C614367F
 
 // Definitions should be global.
@@ -26,3 +27,16 @@
 #include <boost/asio.hpp>
 
 #include <pugixml.hpp>
+
+#include <fmt/format.h>
+
+namespace fmt {
+
+template <typename E>
+constexpr auto underlying(E e) -> std::underlying_type_t<E> {
+    return static_cast<std::underlying_type_t<E>>(e);
+}
+
+}
+
+#endif
